8cc780053a9f0ba781e0eca9e65abe0fe810319f
n.servot@gmail.com <n.servot@gmail.com>
2021-04-19 17:30:09 +0200

    calendar done

--- public/calendar.js
+++ public/calendar.js
@@ -1,21 +1,87 @@
 const date = new Date();
 
 const renderCalendar = () =>{
-    //date.setDate(1);
-    const currentYear=date.getFullYear();
-    const currentMonth = date.getMonth();
-    const currentDay = date.getDate();
-    const currentDatNo = date.getDate();
+    date.setDate(1);
 
-    const lastDay = new Date(currentYear, currentMonth+1, 0).getDate();
-    console.log(lastDay)
+    const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
 
-    const prevLastDay = new Date(currentYear, currentMonth, 0).getDate();
-    console.log(prevLastDay);
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-    
+    const firstDayIndex = date.getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
 
+    const nextDays = ()=>{
+        if(lastDayIndex==6){
+            return 7;
+        }
+        else{
+            return 7-lastDayIndex-1;
+        }
+    }
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+
+      document.querySelector(".date h1").innerHTML= months[date.getMonth()];
+      document.querySelector(".date p").innerHTML = date.toDateString();
+
+      var days="";
+      for(let i =firstDayIndex; i>0; i--){
+        days += `<div class="prev-date">${prevLastDay-i+1}</div>`;
+      }
+
+      for(let i= 1;i<lastDay+1;i++){
+        days += `<div>${i}</div>`;
+
+      }
+      console.log(lastDayIndex);
+
+      for(let i=1; i<=nextDays(); i++){
+        days += `<div class="next-date">${i}</div>`;
+
+      }
+
+      document.querySelector(".days").innerHTML = days;
+
+      var listPrevDays = document.querySelectorAll(".prev-date");
+      for(i=0; i<listPrevDays.length;i++){
+        listPrevDays[i].addEventListener("click", () => {
+            date.setMonth(date.getMonth() -1);
+            renderCalendar();
+        });
+      }
+
+      var listNextDays = document.querySelectorAll(".next-date");
+      for(i=0; i<listNextDays.length;i++){
+        listNextDays[i].addEventListener("click", () => {
+            date.setMonth(date.getMonth() + 1);
+            renderCalendar();
+        });
+      }
 
 };
+
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
+  
+document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+});
+
 
 renderCalendar();
